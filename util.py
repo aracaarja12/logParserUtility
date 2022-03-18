56256425b7ac31f81efcf01b055f13a489a4556f
aracaarja12 <aaronapplegate17@gmail.com>
2022-03-18 01:59:25 -0400

    Implemented -t, -i, and -I with regex

--- util.py
+++ util.py
@@ -2,113 +2,111 @@
 
 import argparse
 import sys
+import re
 
 def parse_cli_args(args): 
-
+	'''
+	Function to parse and error check command line arguments
+		-f,--first and -l,--last consume a single integer above 0
+		-t, -i, and -I are boolean switches
+	Descriptions of each argument are given by the help parameter
+	More verbose descriptions can be found in the README
+	'''
+	
+	# Create the argument parser and define arguments
 	parser = argparse.ArgumentParser(description="CLI application that helps you parse logs of various kinds")
-	parser.add_argument("-f", "--first", metavar="NUM", type=int, nargs=1, help="Print first NUM lines")
-	parser.add_argument("-l", "--last", metavar="NUM", type=int, nargs=1, help="Print last NUM lines")
+	parser.add_argument("-f", "--first", metavar="NUM", type=int, help="Print first NUM lines")
+	parser.add_argument("-l", "--last", metavar="NUM", type=int, help="Print last NUM lines")
 	parser.add_argument("-t", "--timestamp", action="store_true", help="Print lines that contain a timestamp in HH:MM:SS format")
 	parser.add_argument("-i", "--ipv4", action="store_true", help="Print lines that contain an IPv4 address, matching IPs are highlighted")
 	parser.add_argument("-I", "--ipv6", action="store_true", help="Print lines that contain an IPv6 address (standard notation), matching IPs are highlighted")
 	parser.add_argument("file", metavar="FILE", type=argparse.FileType('r'), nargs="?", default=(None if sys.stdin.isatty() else sys.stdin), help="Log file to be parsed")
 	
 	args = parser.parse_args(args)
-	if args.file == None: 
+	
+	# Error checking not automatically handled by argparse
+	if args.file is None: 
 		parser.error("A file or standard input must be provided")
+	if args.first is not None: 
+		if args.first < 1: 
+			parser.error("Numbers supplied to --first must be integers greater than 0")
+	if args.last is not None: 
+		if args.last < 1: 
+			parser.error("Numbers supplied to --last must be integers greater than 0")
+	
 	return args
 
 def calculateBounds(first, last, length): 
+	'''
+	Function to calculate the intersection of the --first and --last arguments
+	Lines of the log that don't fall within this intersection will never be printed
+	This function is intended to simplify computation
+	'''
+	
+	# Assume the entire file will fall within the intersection
+	# Note: this function silently corrects values of --first and --last that exceed the length of the log
 	boundaries = [0,length]
-	print(first)
-	print(last)
-	print(length)
-	if first != None: 
+	if first is not None: 
 		first = length if first > length else first
-	if last != None:
+	if last is not None:
 		last = length if last > length else last
-		
-	# constant operation to find the bounds based on -f and -t if they exist
-	# CASE where neither l or f
-	if first == None and last == None: 
-		# boundaries = [0,len(lines)]
+	
+	# Find the intersection... 
+	if first is None and last is None: # where neither -l or -f is given
+		# boundaries = [0,length]
 		pass
-	# CASE where only f
-	elif last == None:
+	elif last is None: # where only -f is given
 		# boundaries = [0,f]
 		boundaries[1] = first
-	# CASE where only l
-	elif first == None:
-		# boundaries = [len(lines)-l,len(lines)]
+	elif first is None: # where only -l is given
+		# boundaries = [length-l,length]
 		boundaries[0] = length - last
-	# CASE where both l and f
-	else: 
-		# if (l + f) > len(lines), i.e. there is an intersection
-		if first + last > length:
+	else: # where both -l and -f are given... 
+		if first + last > length: # and there is an intersection
 			# boundaries = [length - last, first]
 			boundaries[0] = length - last 
 			boundaries[1] = first
-		# else there is no intersection
-		else: 
+		else: # and there is no intersection
 			boundaries = None
 	
 	return boundaries
-	
-def hasTimestamp(line):
-	pass
-	
-def hasIPv4(line):
-	pass
-	
-def hasIPv6(line):
-	pass
-	
+
+def displaymatch(match): 
+	if match is None: 
+		print("None")
+	print("<Match %r, groups=%r>" % (match.group(),match.groups()))
+
 def main(args): 
+	'''
+	Driver function
+	'''
 	
+	# Parse command line arguments and consume input from the given file or stdin
 	args = parse_cli_args(args)
-	print(args)
-	
 	lines = args.file.readlines()
-	print(lines)
 	args.file.close()
 	
-	first = args.first[0] if args.first != None else None
-	last = args.last[0] if args.last != None else None
-	boundaries = calculateBounds(first,last,len(lines))
-	print("Boundaries are:", boundaries)
+	# Find the intersection of the --first and --last arguments
+	boundaries = calculateBounds(args.first,args.last,len(lines))
 	
+	# Compile regex strings into regex objects to speed up runtime
+	timestamp_re = re.compile(r"([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")
+	ipv4_re = re.compile(r"(([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])")
+	ipv6_re = re.compile(r"([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}")
+	
+	# Print each line in the intersection of the given arguments
 	for line in lines[boundaries[0]:boundaries[1]]: 
-		# t, ipv4, ipv6
 		if args.timestamp:
-			if not hasTimestamp(line):
+			if not timestamp_re.search(line):
 				continue
 		if args.ipv4:
-			if not hasIPv4(line):
+			if not ipv4_re.search(line):
 				continue
 		if args.ipv6:
-			if not hasIPv6(line):
+			if not ipv6_re.search(line):
 				continue
 		print(line)
-		#if inIntersection(line): 
-		#	print(line)
-	
-	# linear operation to iterate through the bounds and verify each row
-		# for each line in the bounds
-			# if args.timestamp == True
-				# if line doesn't have timestamp
-					# continue
-			# if args.ipv4 == True
-				# if line doesn't have ipv4
-					# continue
-			# if args.ipv6 == True
-				# if line doesn't have ipv6
-					# continue
-			# print line
-	
-	# iterate through every line and populate multiple sets based on conditions
-	# find intersection
-	# iterate through the intersection to print
-	
-	
+
+
 if __name__ == "__main__": 
 	main(sys.argv[1:])
