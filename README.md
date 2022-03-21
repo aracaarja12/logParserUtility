66fa98667a722320adf1e604ec79bbc39a1e8d55
aracaarja12 <aaronapplegate17@gmail.com>
2022-03-21 04:34:46 -0400

    Improved documentation in README.md and util.py

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Log Parser Utility
+# Log Parsing Utility
+
+This is a CLI application that helps you parse logs of various kinds. It prints the lines of a given log according to filters set by command line arguments. With the exception of colorama for highlighting IP addresses, the implementation for this uses only Python's standard library. 
+
+In the future, I hope to better optimize this tool and improve upon its IPv6 detection. Presently, logParserUtility is only capable of detecting IPv6 addresses in standard form that haven't been shortened with double colons, and I'd like to improve upon this. 
+
+## Structure and contents
+
+logParserUtility/
+|-- .gitignore
+|-- LICENSE
+|-- README.md
+|-- requirements.txt
+|-- testLogs/
+|   |-- various logs for testing
+|-- tests.py
+|-- util.py
+
+util.py -- the application
+tests.py -- test suite for the application
+
+## How to install and run
+
+This application has one dependency, colorama. You can either install it directly or run the following command to make use of the supplied `requirements.txt` file. 
+
+`pip install -r requirements.txt`
+
+A shebang is included in `util.py` to allow for execution without explicitly calling `python`. Usage follows: 
+
+./util.py \[-h\] \[-f NUM\] \[-l NUM\] \[-t\] \[-i\] \[-I\] \[FILE\]
+
+| Argument | Description |
+| -h, --help | show this help message and exit |
+| -f NUM, --first NUM | print first NUM lines |
+| -l NUM, --last NUM | print last NUM lines |
+| -t, --timestamps | print lines that contain a timestamp in HH:MM:SS format |
+| -i, --ipv4 | print lines that contain an IPv4 address, matching IPs are highlighted |
+| -I, --ipv6 | print lines that contain an IPv6 address (standard notation), matching IPs are highlighted |
+| FILE | log file to be parsed |
+
+All arguments are optional, but the result is the intersection of the arguments' results, so no output will be generated if no arguments are used. If FILE is omitted, standard input is used instead. 
+
+## Usage examples
+
+./util.py -h
+<prints help>
+
+cat test_0.log | ./util.py --first 10
+<prints the first 10 lines of test_0.log>
+
+./util.py -l 5 test_1.log
+<prints the last 5 lines of test_1.log>
+
+./utils.py --timestamps test_2.log
+<prints any lines from test_2.log that contain a timestamp>
+
+./util.py --ipv4 test_3.log
+<prints any lines from test_3.log that contain an IPv4 address>
+
+./util.py --ipv6 test_4.log
+<prints any lines from test_4.log that contain an IPv6 address>
+
+./util.py --ipv4 --last 50 test_5.log
+<prints any of the last 50 lines from test_5.log that contain an IPv4
+address>
+
+
+## Testing
+
--- util.py
+++ util.py
@@ -1,10 +1,9 @@
-#!C:\Users\Aaron\AppData\Local\Programs\Python\Python39\python.exe
+#!/usr/bin/env python3
 
 import argparse
 import sys
 import re
 from colorama import init, Back, Style
-from operator import itemgetter
 
 def parse_cli_args(args): 
 	'''
@@ -17,12 +16,12 @@ def parse_cli_args(args):
 	
 	# Create the argument parser and define arguments
 	parser = argparse.ArgumentParser(description="CLI application that helps you parse logs of various kinds")
-	parser.add_argument("-f", "--first", metavar="NUM", type=int, help="Print first NUM lines")
-	parser.add_argument("-l", "--last", metavar="NUM", type=int, help="Print last NUM lines")
-	parser.add_argument("-t", "--timestamps", action="store_true", help="Print lines that contain a timestamp in HH:MM:SS format")
-	parser.add_argument("-i", "--ipv4", action="store_true", help="Print lines that contain an IPv4 address, matching IPs are highlighted")
-	parser.add_argument("-I", "--ipv6", action="store_true", help="Print lines that contain an IPv6 address (standard notation), matching IPs are highlighted")
-	parser.add_argument("file", metavar="FILE", type=argparse.FileType('r'), nargs="?", default=(None if sys.stdin.isatty() else sys.stdin), help="Log file to be parsed")
+	parser.add_argument("-f", "--first", metavar="NUM", type=int, help="print first NUM lines")
+	parser.add_argument("-l", "--last", metavar="NUM", type=int, help="print last NUM lines")
+	parser.add_argument("-t", "--timestamps", action="store_true", help="print lines that contain a timestamp in HH:MM:SS format")
+	parser.add_argument("-i", "--ipv4", action="store_true", help="print lines that contain an IPv4 address, matching IPs are highlighted")
+	parser.add_argument("-I", "--ipv6", action="store_true", help="print lines that contain an IPv6 address (standard notation), matching IPs are highlighted")
+	parser.add_argument("file", metavar="FILE", type=argparse.FileType('r'), nargs="?", default=(None if sys.stdin.isatty() else sys.stdin), help="log file to be parsed")
 	
 	args = parser.parse_args(args)
 	
@@ -56,17 +55,14 @@ def calculateBounds(first, last, length):
 		elif last < 0: 
 			last = abs(last)
 	
-	# Find the intersection... 
-	if first is None and last is None: # where neither -l or -f is given
-		# boundaries = [0,length]
-		pass
-	elif last is None: # where only -f is given
+	# Edit the intersection... 
+	if first is not None and last is None: # if only -f is given
 		# boundaries = [0,f]
 		boundaries[1] = first
-	elif first is None: # where only -l is given
+	elif first is None and last is not None: # if only -l is given
 		# boundaries = [length-l,length]
 		boundaries[0] = length - last
-	else: # where both -l and -f are given... 
+	else: # if both -l and -f are given... 
 		if first + last > length: # and there is an intersection
 			# boundaries = [length - last, first]
 			boundaries[0] = length - last 
@@ -76,14 +72,11 @@ def calculateBounds(first, last, length):
 	
 	return boundaries
 
-'''
-def displaymatch(match): 
-	if match is None: 
-		print("None")
-	print("<Match %r, groups=%r>" % (match.group(),match.groups()))
-'''
-
 def splitByIdx(line, indices): 
+	'''
+	Generator that splits a string by a list of indices
+	'''
+	
 	front = 0
 	back = indices[0]
 	yield line[front:back]
@@ -94,7 +87,12 @@ def splitByIdx(line, indices):
 	yield line[front:]
 
 def highlightIPs(line, matches): 
-	# split line into a list
+	'''
+	Function to add highlighting to a string given a list of re.Match objects
+	Returns the altered string
+	'''
+	
+	# Split line into a list based on the given regex matches
 	indices = []
 	startsWithIP = False
 	for match in matches: 
@@ -105,18 +103,13 @@ def highlightIPs(line, matches):
 				indices.append(i)
 	segments = [*splitByIdx(line,indices)]
 	
-	# color list elements that are IPs
+	# Color list elements that are IPs
 	startingPosition = 0 if startsWithIP else 1
 	for i in range(startingPosition,len(segments),2): 
 		segments[i] = Back.GREEN + segments[i] + Style.RESET_ALL
 	
-	# combine list into one string and return
+	# Combine list into one string and return
 	return "".join(segments)
-
-def testhighlight(): 
-	s = "up what's up doc up doc up up up"
-	iter = re.compile("up").finditer(s)
-	print(highlightIPs(s,iter))
 
 def main(args): 
 	'''
@@ -139,7 +132,6 @@ def main(args):
 	ipv6_re = re.compile(r"([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}")
 	
 	# True if lines in the intersection will contain IP addresses that must be highlighted
-	#IPs_included = args.ipv4 or args.ipv6
 	if args.ipv4 or args.ipv6: 
 		init()
 	IP_match = None
@@ -155,18 +147,13 @@ def main(args):
 				continue
 			line = highlightIPs(line, IP_match)
 		if args.ipv6:
-			#IP_match = ipv6_re.search(line)
 			IP_match = [*ipv6_re.finditer(line)]
 			if not IP_match:
 				continue
 			line = highlightIPs(line, IP_match)
-		
-		#if IP_match: 
-			#line = line[:IP_match.span()[0]] + Back.GREEN + line[IP_match.span()[0]:IP_match.span()[1]] + Style.RESET_ALL + line[IP_match.span()[1]:]
 		
 		print(line,end="")
 
 
 if __name__ == "__main__": 
 	main(sys.argv[1:])
-	#testhighlight()
