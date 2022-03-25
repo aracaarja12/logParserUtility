fbf86ac28454007177f34f9eb1815ff324b74a8a
aracaarja12 <aaronapplegate17@gmail.com>
2022-03-25 02:08:47 -0400

    Added coverage for combinations of options in -t, -i, and -I

--- tests/test_timestampsAndIPs.py
+++ tests/test_timestampsAndIPs.py
@@ -10,76 +10,15 @@ sys.path.append(parent)
 import util
 
 '''
-Fixtures
-'''
-
-@pytest.fixture
-def timestamp_log(): 
-	'''
-	Returns the text of testLogs/test_timestamp.log
-	'''
-	
-	return (
-		"Line 1 04:06:15 valid\n",
-		"Line 2 4:06:15 invalid\n",
-		"14:59:06 Line 3 valid\n",
-		"Line 4 14:70:06 invalid\n",
-		"Line 5 valid 18:30:24\n",
-		"Line 6 20:30 invalid\n",
-		"Line 7 valid 23:59:59\n",
-		"Line 8 invalid 20:04:100\n",
-		"Line 9 valid 00:00:00\n",
-		"Line 10 invalid00:04:10\n"
-	)
-
-@pytest.fixture
-def ipv4_log(): 
-	'''
-	Returns the text of testLogs/test_ipv4.log
-	'''
-	
-	return (
-		"Line 1 192.168.255.0 valid\n",
-		"Line 2 192.260.255.0 invalid\n",
-		"86.115.8.11 Line 3 valid\n",
-		"Line 4 234.98.125.251invalid\n",
-		"Line 5 valid 234.098.125.251\n",
-		"Line 6234.98.125.251 invalid\n",
-		"Line 7 valid 0.0.0.0\n",
-		"Line 8 invalid 43.119.240.-72\n",
-		"Line 9 valid 255.255.255.255\n",
-		"Line 10 invalid 331.101.157"
-	)
-
-@pytest.fixture
-def ipv6_log(): 
-	'''
-	Returns the text of testLogs/test_ipv6.log
-	'''
-	
-	return (
-		"Line 1 cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80 valid\n",
-		"Line 2 cc13:467e:88db:a4b0:fc68:dd9d:5a9e invalid\n",
-		"CF91:B4ED:13FB:1857:6B32:CB99:54E8:82BC Line 3 valid\n",
-		"Line 4 cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80invalid\n",
-		"Line 5 valid Cf91:B4Ed:13Fb:1857:6B32:cB99:54e8:82Bc\n",
-		"Line 6cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80 invalid\n",
-		"Line 7 valid 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd\n",
-		"Line 8 invalid cc13:467e:88db:a4b0000:fc68:dd9d:5a9e:ab80\n",
-		"Line 9 valid FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF\n",
-		"Line 10 invalid cc13:467e:88dG:a4b0:fc68:dd9d:5a9e:ab80"
-	)
-
-'''
 Helpers
 '''
 
+'''
 def get_expected_lines(log_text, idx): 
-	'''
-	Returns expected text from the given log based on a list of lines that should be printed
-	'''
+	#Returns expected text from the given log based on a list of lines that should be printed
 	
 	return "".join([log_text[i] for i in idx if i < len(log_text)  and i >= 0])
+'''
 
 def get_expected_values_for_erroneous_args(extra_arg): 
 	'''
@@ -96,58 +35,85 @@ def get_expected_values_for_erroneous_args(extra_arg):
 Positive tests
 '''
 
-@pytest.mark.parametrize("switch", ["-t", "--timestamp"])
-def test_timestamp_only(capsys, timestamp_log, switch): 
+def test_timestamp_only(capsys): 
 	'''
 	Tests -t independently with a log containing valid and invalid timestamps (valid timestamps are of the form HH:MM:SS)
 	'''
 	
-	args = [switch, "testLogs/test_timestamp.log"]
+	args = ["--timestamp", "testLogs/test_timestamp.log"]
 	util.main(args)
 	captured = capsys.readouterr()
-	expected = get_expected_lines(timestamp_log, [i for i in range(0, 10, 2)])
+	expected = "Line 1 04:06:15 valid\n14:59:06 Line 3 valid\nLine 5 valid 18:30:24\nLine 7 valid 23:59:59\nLine 9 valid 00:00:00\n12:05:35 22:59:05 00:03:12"
 	assert captured.out == expected
 	assert captured.err == ""
 
-@pytest.mark.parametrize("switch", ["-i", "--ipv4"])
-def test_ipv4_only(capsys, ipv4_log, switch): 
+def test_ipv4_only(capsys): 
 	'''
 	Tests -i independently with a log containing valid and invalid IPv4 addresses (valid IPs are of the form xxx.xxx.xxx.xxx where xxx is in (0,255))
 	Note: this does not verify IP highlighting
 	'''
 	
-	args = [switch, "testLogs/test_ipv4.log"]
+	args = ["--ipv4", "testLogs/test_ipv4.log"]
 	util.main(args)
 	captured = capsys.readouterr()
-	expected = get_expected_lines(ipv4_log, [i for i in range(0, 10, 2)])
+	expected = "Line 1 192.168.255.0 valid\n86.115.8.11 Line 3 valid\nLine 5 valid 234.098.125.251\nLine 7 valid 0.0.0.0\nLine 9 valid 255.255.255.255\n192.168.255.0 86.115.8.11 43.119.240.72"
 	assert captured.out == expected
 	assert captured.err == ""
 
-@pytest.mark.parametrize("switch", ["-I", "--ipv6"])
-def test_ipv6_only(capsys, ipv6_log, switch): 
+def test_ipv6_only(capsys): 
 	'''
 	Tests -I independently with a log containing valid and invalid IPv6 addresses (valid IPs are of the form yyyy:yyyy:yyyy:yyyy:yyyy:yyyy:yyyy:yyyy where y is in hex (0-F))
 	Note: this does not verify IP highlighting
 	'''
 	
-	args = [switch, "testLogs/test_ipv6.log"]
+	args = ["--ipv6", "testLogs/test_ipv6.log"]
 	util.main(args)
 	captured = capsys.readouterr()
-	expected = get_expected_lines(ipv6_log, [i for i in range(0, 10, 2)])
+	expected = "Line 1 cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80 valid\nCF91:B4ED:13FB:1857:6B32:CB99:54E8:82BC Line 3 valid\nLine 5 valid Cf91:B4Ed:13Fb:1857:6B32:cB99:54e8:82Bc\nLine 7 valid 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd\nLine 9 valid FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF\n2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80 0000:0000:0000:0000:0000:0000:0000:0000"
 	assert captured.out == expected
 	assert captured.err == ""
 
-def test_timestamp_and_ipv4(): 
-	pass
+def test_timestamp_and_ipv4(capsys): 
+	'''
+	Tests -t in conjunction with -i
+	'''
+	
+	args = ["-t", "-i", "testLogs/test_general.log"]
+	util.main(args)
+	captured = capsys.readouterr()
+	expected = "Line 2: 02:49:12 172.16.254.1 ti\nLine 3: 192.168.255.0 02:38:00 127.0.0.1 ti\nLine 5: 127.255.0.10 23:59:59 192.168.255.255 0aF3:AF18:1762:B03:0:1:af19:17f tiI\nLine 12: 00:000:00:000:00:000:00:00 0.0.0.0 00:00:00 tiI\nLine 13: 09:17:17 : 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd -- 234.098.125.251 200 tiI\nLine 15: 127.127.127.127 abcd:1234:ab:12:abcd:1234:cd:34 03:01:56 tiI\nLine 17: 119.243.4.69 88.133.63.210 17:59:00 17c8:d4ea:1cb2:afeb:d2b9:945e:6e6e:9152 tiI\nLine 20: 04:52:24 144.1.18.87 ti"
+	assert captured.out == expected
+	assert captured.err == ""
 
-def test_timestamp_and_ipv6(): 
-	pass
+def test_timestamp_and_ipv6(capsys): 
+	args = ["-t", "-I", "testLogs/test_general.log"]
+	util.main(args)
+	captured = capsys.readouterr()
+	expected = "Line 5: 127.255.0.10 23:59:59 192.168.255.255 0aF3:AF18:1762:B03:0:1:af19:17f tiI\nLine 9: Cf91:B4Ed:13Fb:1857:6B32:cB99:54e8:82Bc 00:00:00 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd tI\nLine 10: cc13:467e:88db:a4b0:fc68:dd9d:5a9e:ab80 06:15:04 tI\nLine 12: 00:000:00:000:00:000:00:00 0.0.0.0 00:00:00 tiI\nLine 13: 09:17:17 : 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd -- 234.098.125.251 200 tiI\nLine 15: 127.127.127.127 abcd:1234:ab:12:abcd:1234:cd:34 03:01:56 tiI\nLine 17: 119.243.4.69 88.133.63.210 17:59:00 17c8:d4ea:1cb2:afeb:d2b9:945e:6e6e:9152 tiI\nLine 19: f3ba:559b:77b1:8fd6:d46c:ffc8:dc92:5f7c 14:25:42 tI\n"
+	assert captured.out == expected
+	assert captured.err == ""
 
-def test_ipv4_and_ipv6(): 
-	pass
+def test_ipv4_and_ipv6(capsys): 
+	args = ["-i", "-I", "testLogs/test_general.log"]
+	util.main(args)
+	captured = capsys.readouterr()
+	expected = "Line 5: 127.255.0.10 23:59:59 192.168.255.255 0aF3:AF18:1762:B03:0:1:af19:17f tiI\nLine 12: 00:000:00:000:00:000:00:00 0.0.0.0 00:00:00 tiI\nLine 13: 09:17:17 : 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd -- 234.098.125.251 200 tiI\nLine 14: 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd -- 234.098.125.251 200 iI\nLine 15: 127.127.127.127 abcd:1234:ab:12:abcd:1234:cd:34 03:01:56 tiI\nLine 16: 6a03:b896:63fd:c4a2:92d8:16d6:99bf:b429 37.196.62.32 iI\nLine 17: 119.243.4.69 88.133.63.210 17:59:00 17c8:d4ea:1cb2:afeb:d2b9:945e:6e6e:9152 tiI\nLine 18: 162.181.128.181 28e4:aaa0:54f4:2de6:dc4a:916f:7cb8:b541 iI\n"
+	assert captured.out == expected
+	assert captured.err == ""
 
-def test_timestamp_and_ipv4_and_ipv6(): 
-	pass
+def test_timestamp_and_ipv4_and_ipv6(capsys): 
+	args = ["-t", "-i", "-I", "testLogs/test_general.log"]
+	util.main(args)
+	captured = capsys.readouterr()
+	expected = "Line 5: 127.255.0.10 23:59:59 192.168.255.255 0aF3:AF18:1762:B03:0:1:af19:17f tiI\nLine 12: 00:000:00:000:00:000:00:00 0.0.0.0 00:00:00 tiI\nLine 13: 09:17:17 : 2bc2:2a2d:98:dc3:0:18e0:aa7c:c0bd -- 234.098.125.251 200 tiI\nLine 15: 127.127.127.127 abcd:1234:ab:12:abcd:1234:cd:34 03:01:56 tiI\nLine 17: 119.243.4.69 88.133.63.210 17:59:00 17c8:d4ea:1cb2:afeb:d2b9:945e:6e6e:9152 tiI\n"
+	assert captured.out == expected
+	assert captured.err == ""
+
+# TODO add coverage for -f and -l combined with some combination of -t, -i, and -I
+
+# TODO add coverage for IP highlighting (ie unit tests for highlight_ip_addresses())
+
+# TODO add unit tests for split_by_idx
 
 '''
 Negative tests
